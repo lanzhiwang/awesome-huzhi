4d1c28e60b793e91d8ca83f970f0b1cba7d34de8
daocloud <daocloud@huzhideMacBook-Pro.local>
2019-09-20 15:29:45 +0800

    python 多进程处理数据库操作

--- python/DB_pool/pool.py
+++ python/DB_pool/pool.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import multiprocessing, time, psycopg2
 
 class Consumer(multiprocessing.Process):
@@ -6,6 +8,7 @@ class Consumer(multiprocessing.Process):
         multiprocessing.Process.__init__(self)
         self.task_queue = task_queue
         self.result_queue = result_queue
+        # todo 使用连接池
         self.pyConn = psycopg2.connect("dbname='geobase_1' host = 'localhost'")
         self.pyConn.set_isolation_level(0)
 
@@ -32,16 +35,21 @@ class Task(object):
         pyConn = connection
         pyCursor1 = pyConn.cursor()
 
+        # todo 数据库的具体操作不要写死，通过参数传递
         procQuery = 'UPDATE city SET gid_fkey = gid FROM country  WHERE ST_within((SELECT the_geom FROM city WHERE city_id = %s), country.the_geom) AND city_id = %s' % (self.a, self.a)
-
         pyCursor1.execute(procQuery)
         print 'What is self?'
         print self.a
 
         return self.a
 
+        # todo 任务是否是同一类型，要么全部是查询，要么全部是插入，要么全部是更新，要么全部是删除
+        # todo 不同类型的任务怎么办
+        # todo 数据库客户端是否是异步的
+
     def __str__(self):
         return 'ARC'
+
     def run(self):
         print 'IN'
 
